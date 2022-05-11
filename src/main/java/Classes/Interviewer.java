fbeea028d6b76ea99812f07f83d37a63b5412ae5
OWLL75 <7soony2818@gmail.com>
2022-05-11 23:17:15 +0300

    interview change

--- src/main/java/Classes/Interviewer.java
+++ src/main/java/Classes/Interviewer.java
@@ -3,11 +3,23 @@ package Classes;
 import java.util.ArrayList;
 
 public class Interviewer {
-   ArrayList<Interview>listOfJobs;
-   String name;
+	ArrayList<Interview>listOfJobs;
+	String name;
 
-   public Interviewer(String name){
-      this.name = name;
-   }
-   public String getName(){return name;}
+	public Interviewer(String name){
+		this.name = name;
+	}
+	public String getName(){return name;}
+
+	public void setResult(int ID, String result1) {
+		for (int i = 0; i < listOfJobs.size(); i++) {
+			if(listOfJobs.get(i).getID() == ID) {
+				listOfJobs.get(i).setResult(result1);
+			}
+		}
+	}
+
+	public ArrayList<Interview> getlistOfJobs(){
+		return listOfJobs;
+	}
 }
